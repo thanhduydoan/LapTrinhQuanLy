0e3b8b4a2857d968f6fa565120dc996317de57b3
DESKTOP-RAAIMNI\ACER <exanp>
2022-03-01 09:36:26 +0700

    Add_Class_Student

---
+++ DDTBaiThucHanh486/DDTBaiThucHanh486/Models/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DDTBaiThucHanh486.Models
+{
+    public class Student
+    {
+        [Key]
+        public string MaSinhVien { get; set; }
+        public string TenSinhVien { get; set; }
+    }
+}
